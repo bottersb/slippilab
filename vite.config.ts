99f2c20e6f3506eafed2866d43cc709301ad4b2a
Benjamin Ottersbach <benjamin.ottersbach@gmx.de>
2024-11-13 23:36:58 +0100

    May use different root path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import solidPlugin from "vite-plugin-solid";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: './',
   assetsInclude: [/.*zip$/, /.*ttf$/],
   plugins: [viteTsconfigPaths(), solidPlugin()],
   resolve: {
